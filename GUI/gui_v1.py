36e376b0416cd0fae8c53d0097ca706d897e1fc7
cesarz <cesarz@ksu.edu>
2021-11-30 04:17:58 -0600

    new GUI, new dispensecheck

--- GUI/gui_v1.py
+++ GUI/gui_v1.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 from tkinter import *
 import tkinter as tk
-import tkinter.font as font
 from tkinter import ttk
+import tkinter.font as font
 import sys
 import time
 import RPi.GPIO as GPIO
@@ -41,8 +41,6 @@ monthDict = {
 }
 bracelet_MAC_address = ""
 currentlyDispensing = False
-dispenseTimer = 0
-dispensedPills = 0
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -188,80 +186,9 @@ class GuiPart:
     #############################################
 
 
-    def arrowButtons(self, row, direction):
-        if row == 0:
-            if direction == 'up' and monthDict[self.start_month.get()] <= 11:
-                self.start_month.current(monthDict[self.start_month.get()])
-            elif direction == 'down' and monthDict[self.start_month.get()]-2 >= 0:
-                self.start_month.current(monthDict[self.start_month.get()]-2)
-        elif row == 1:
-            if direction == 'up' and int(self.start_day.get())-1 <= len(self.start_day['values'])-1:
-                self.start_day.current(int(self.start_day.get())-1)
-            elif direction == 'down' and int(self.start_day.get())-3 >= 0:
-                self.start_day.current(int(self.start_day.get())-3)
-        elif row == 2:
-            if direction == 'up' and monthDict[self.end_month.get()] <= 11:
-                self.end_month.current(monthDict[self.end_month.get()])
-            elif direction == 'down' and monthDict[self.end_month.get()]-2 >= 0:
-                self.end_month.current(monthDict[self.end_month.get()]-2)
-        elif row == 3:
-            if direction == 'up' and int(self.end_day.get())-1 <= len(self.end_day['values'])-1:
-                self.end_day.current(int(self.end_day.get())-1)
-            elif direction == 'down' and int(self.end_day.get())-3 >= 0:
-                self.end_day.current(int(self.end_day.get())-3)
-        elif row == 4:
-            if direction == 'up' and int(self.hour.get()) <= 11:
-                self.hour.current(int(self.hour.get()))
-            elif direction == 'down' and int(self.hour.get())-2 >= 0:
-                self.hour.current(int(self.hour.get())-2)
-        elif row == 5:
-            newMin = int(self.minute.get())
-            if direction == 'up':
-                if newMin == 0:
-                    self.minute.current(1)
-                elif newMin == 15:
-                    self.minute.current(2)
-                elif newMin == 30:
-                    self.minute.current(3)
-                elif newMin == 45:
-                    self.minute.current(4)
-            elif direction == 'down':
-                if newMin == 0:
-                    self.minute.current(0)
-                elif newMin == 15:
-                    self.minute.current(0)
-                elif newMin == 30:
-                    self.minute.current(1)
-                elif newMin == 45:
-                    self.minute.current(2)
-                else:
-                    self.minute.current(3)
-        elif row == 6:
-            if direction == 'up':
-                self.am_pm.current(1)
-            elif direction == 'down':
-                self.am_pm.current(0)
-        elif row == 7:
-            if direction == 'up' and int(self.repeat.get())+1 <= 11:
-                self.repeat.current(int(self.repeat.get())+1)
-            elif direction == 'down' and int(self.repeat.get())-1 >= 0:
-                self.repeat.current(int(self.repeat.get())-1)
-        elif row == 8:
-            if direction == 'up' and int(self.pills.get()) <= 4:
-                self.pills.current(int(self.pills.get()))
-            elif direction == 'down' and int(self.pills.get())-2 >= 0:
-                self.pills.current(int(self.pills.get())-2)
-
-
-        
-
-
-
-
     # Schedule page
     def New_Window(self):
         
-        # open up a new window
         myFont_w = font.Font(size=16)
         
         # open up a new window
@@ -286,11 +213,11 @@ class GuiPart:
         self.start_month.grid(row=0, column=2)
         self.start_month.current(int(time.strftime("%m"))-1)
         
-        button_0_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(0, 'down'))
+        button_0_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_0_0 ['font'] = myFont_w
         button_0_0.grid(row = 0, column = 3)
         
-        button_0_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(0, 'up'))
+        button_0_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_0_1.grid(row = 0, column = 4)
         button_0_1 ['font'] = myFont_w
 
@@ -305,11 +232,11 @@ class GuiPart:
         self.change_startdays()        
         self.start_day.current(int(time.strftime("%d"))-2)
         
-        button_1_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(1, 'down'))
+        button_1_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_1_0 ['font'] = myFont_w
         button_1_0.grid(row = 1, column = 3)
         
-        button_1_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(1, 'up'))
+        button_1_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_1_1 ['font'] = myFont_w
         button_1_1.grid(row = 1, column = 4)
         
@@ -322,13 +249,13 @@ class GuiPart:
         self.end_month = ttk.Combobox(Window, values=["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"], height = 9, width = 14)
         self.end_month['font'] = myFont_w
         self.end_month.grid(row=2, column=2)
-        self.end_month.current(int(time.strftime("%m"))-1)
+        self.end_month.current(0)
         
-        button_2_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(2, 'down'))
+        button_2_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_2_0 ['font'] = myFont_w
         button_2_0.grid(row = 2, column = 3)
         
-        button_2_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(2, 'up'))
+        button_2_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_2_1 ['font'] = myFont_w
         button_2_1.grid(row = 2, column = 4)
         
@@ -341,13 +268,12 @@ class GuiPart:
         self.end_day.grid(row=3, column=2)    
         
         self.change_enddays()
-        self.end_day.current(int(time.strftime("%d"))-2)
         
-        button_3_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(3, 'down'))
+        button_3_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_3_0 ['font'] = myFont_w
         button_3_0.grid(row = 3, column = 3)
         
-        button_3_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(3, 'up'))
+        button_3_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_3_1 ['font'] = myFont_w
         button_3_1.grid(row = 3, column = 4)
         
@@ -356,16 +282,16 @@ class GuiPart:
         label_time['font'] = myFont_w
         label_time.grid(row=4, column=1)
             
-        self.hour = ttk.Combobox(Window, values=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"], height = 9, width=14)
+        self.hour = ttk.Combobox(Window, values=["1", "2", "4", "5", "6", "7", "8", "9", "10", "11", "12"], height = 9, width=14)
         self.hour['font'] = myFont_w
         self.hour.grid(row=4, column=2)
-        self.hour.current(int(time.strftime("%I"))-1)
+        self.hour.current(0)
         
-        button_4_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(4, 'down'))
+        button_4_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_4_0 ['font'] = myFont_w
         button_4_0.grid(row = 4, column = 3)
         
-        button_4_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(4, 'up'))
+        button_4_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_4_1 ['font'] = myFont_w
         button_4_1.grid(row = 4, column = 4)
         
@@ -382,11 +308,11 @@ class GuiPart:
         self.minute.grid(row=5, column=2)
         self.minute.current(0)
         
-        button_5_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(5, 'down'))
+        button_5_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_5_0 ['font'] = myFont_w
         button_5_0.grid(row = 5, column = 3)
         
-        button_5_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(5, 'up'))
+        button_5_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_5_1 ['font'] = myFont_w
         button_5_1.grid(row = 5, column = 4)
         
@@ -400,11 +326,11 @@ class GuiPart:
         self.am_pm.grid(row=6, column=2)
         self.am_pm.current(0)
         
-        button_6_0 = tk.Button(Window, text="AM", height = 2, width = 10, command=lambda: self.arrowButtons(6, 'down'))
+        button_6_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_6_0 ['font'] = myFont_w
         button_6_0.grid(row = 6, column = 3)
         
-        button_6_1 = tk.Button(Window, text="PM", height = 2, width = 10, command=lambda: self.arrowButtons(6, 'up'))
+        button_6_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_6_1 ['font'] = myFont_w
         button_6_1.grid(row = 6, column = 4)
         
@@ -412,16 +338,16 @@ class GuiPart:
         label_repeat['font'] = myFont_w
         label_repeat.grid(row=7, column=1)
         
-        self.repeat = ttk.Combobox(Window, values=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"], height=9, width=14)
-        self.repeat['font'] = myFont_w
-        self.repeat.grid(row=7, column=2)
-        self.repeat.current(0)
+        repeat = ttk.Combobox(Window, values=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"], height=9, width=14)
+        repeat['font'] = myFont_w
+        repeat.grid(row=7, column=2)
+        repeat.current(0)
         
-        button_7_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(7, 'down'))
+        button_7_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_7_0 ['font'] = myFont_w
         button_7_0.grid(row = 7, column = 3)
         
-        button_7_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(7, 'up'))
+        button_7_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_7_1 ['font'] = myFont_w
         button_7_1.grid(row = 7, column = 4)
 
@@ -429,16 +355,16 @@ class GuiPart:
         label_number['font'] = myFont_w
         label_number.grid(row=8, column=1)
         
-        self.pills = ttk.Combobox(Window, values=["1", "2", "3", "4", "5"], height=9, width=14)
-        self.pills['font'] = myFont_w
-        self.pills.grid(row=8, column=2)
-        self.pills.current(0)
+        numberOfPills = ttk.Combobox(Window, values=["1", "2", "3", "4", "5"], height=9, width=14)
+        numberOfPills['font'] = myFont_w
+        numberOfPills.grid(row=8, column=2)
+        numberOfPills.current(0)
         
-        button_8_0 = tk.Button(Window, text="DOWN", height = 2, width = 10, command=lambda: self.arrowButtons(8, 'down'))
+        button_8_0 = tk.Button(Window, text="up", height = 2, width = 10)
         button_8_0 ['font'] = myFont_w
         button_8_0.grid(row = 8, column = 3)
         
-        button_8_1 = tk.Button(Window, text="UP", height = 2, width = 10, command=lambda: self.arrowButtons(8, 'up'))
+        button_8_1 = tk.Button(Window, text="down", height = 2, width = 10)
         button_8_1 ['font'] = myFont_w
         button_8_1.grid(row = 8, column = 4)
 
@@ -465,6 +391,7 @@ class GuiPart:
         self.confirm_time_end['font'] = myFont_w
         self.confirm_time_end.grid(row=5, column=0)
         
+        
     def confirm(self):
         self.confirm_time_start.config(text="Start:" + self.start_month.get() + "," + self.start_day.get() + "," + self.hour.get() + ":" + self.minute.get() + "," + self.am_pm.get())
         self.confirm_time_end.config(text="End:" + self.end_month.get() + "," + self.end_day.get() + "," + self.hour.get() + ":" + self.minute.get() + "," + self.am_pm.get())
@@ -479,7 +406,7 @@ class GuiPart:
             start_year = int(time.strftime("%Y"))
         print(start_year)
 
-        newItem = dispenseItem(uuid.uuid4().hex, start_year, int(monthDict[self.start_month.get()]), int(self.start_day.get()), (str(timeAdjusted) + ":" + self.minute.get()), False, int(self.pills.get()))
+        newItem = dispenseItem(uuid.uuid4().hex, start_year, int(monthDict[self.start_month.get()]), int(self.start_day.get()), (str(timeAdjusted) + ":" + self.minute.get()))
         self.dispenseEvents.append(newItem)
         self.queue.put('update')
         print("Confirmed Event: " + "Start day:" + self.start_month.get() + ", " + self.start_day.get() + ", " + self.hour.get() + ":" + self.minute.get() + ", " + self.am_pm.get() + "   End day:" + self.end_month.get() + ", " + self.end_day.get() + ", " + self.hour.get() + ":" + self.minute.get() + ", " + self.am_pm.get())
@@ -514,22 +441,23 @@ class GuiPart:
         self.end_day.after(200, self.change_enddays)
 
     def __init__(self, ws, queue, endCommand, dispEvents, clearEvents):
-
+        
+        myFont_m = font.Font(size=15)
         # Object to keep track of dispensing events.
         self.dispenseEvents = dispEvents
 
         self._is_running = True
         self.queue = queue
         # Set up the GUI
-        ws.geometry("800x480")
-        # ws.attributes("-fullscreen", True)
+        # ws.geometry("800x480")
+        ws.attributes("-fullscreen", True)
 
         # current clock display
-        self.clock_disp = tk.Label(ws, text="", font=("Helvetica", 30), fg="white", bg="black")
+        self.clock_disp = tk.Label(ws, text="", font=("Helvetica", 40), fg="white", bg="black")
         self.clock_disp.pack()
 
         # show the exact date
-        self.clock_disp1 = tk.Label(ws, text="", font=("Helvetica", 25), fg="white", bg="black")
+        self.clock_disp1 = tk.Label(ws, text="", font=("Helvetica", 30), fg="white", bg="black")
         self.clock_disp1.pack()        
         self.clock()
 
@@ -537,7 +465,7 @@ class GuiPart:
         space.pack()
 
         # upcoming events display
-        notice = tk.Label(ws, text="Upcoming events", font=("Helvetica", 12))
+        notice = tk.Label(ws, text="Upcoming events", font=("Helvetica", 25))
         notice.pack()
 
         self.events = tk.Listbox(ws, height = 10, width = 45, activestyle = 'none')
@@ -547,7 +475,8 @@ class GuiPart:
         space_1 = tk.Label(ws, text=" ")
         space_1.pack()
 
-        button_clear = tk.Button(ws, text="Clear events", command=clearEvents, bg='White', fg='Black')
+        button_clear = tk.Button(ws, text="Clear events", command=clearEvents, bg='White', fg='Black', height=2, width=14)
+        button_clear['font'] = myFont_m
         button_clear.pack()
 
         space_2 = tk.Label(ws, text=" ")
@@ -555,11 +484,12 @@ class GuiPart:
 
         # schedule button jump to the schdule page
         button = tk.Button(ws, text="Schedule", bg='White', fg='Black',
-                                      command=lambda:self.New_Window())
-
+                                      command=lambda:self.New_Window(), height=2, width=14)
+        button['font'] = myFont_m
         button.pack()
 
-        close_main = tk.Button(ws, text="Close", command=endCommand, bg='White', fg='Black')
+        close_main = tk.Button(ws, text="Close", command=endCommand, bg='White', fg='Black', height=2, width=14)
+        close_main['font'] = myFont_m
         close_main.pack()
 
     def processIncoming(self, clearEvents):
@@ -585,10 +515,8 @@ class GuiPart:
                     if not currentlyDispensing and msg in self.dispenseEvents:
                         currentlyDispensing = True
 
-
-                        # dispensing actions and checks
+                        # dispensing actions placeholder
                         dispense(3/1000.0,128)
-
 
                         #Remove used dispenseItem
                         clearEvents('item', msg)
@@ -696,54 +624,16 @@ class ThreadedClient:
         the time inside every dispenseItem inside DispenseEvents,
         and if it matches, we send the dispense signal into the queue.
         """
-        currentItem = []
         while self.running:
             print('Checking dispense time')
             currentTime = time.strftime("%H") + ":" + time.strftime("%M")
             for item in self.dispenseEvents:
                 if not currentlyDispensing and int(time.strftime("%d")) >= item.start_day and int(time.strftime("%m")) >= item.start_month:
                     if currentTime == item.dispenseTime:
-                        currentItem = item
-                        currentlyDispensing = True
-                        self.queue.put('dispensing')
+                        self.queue.put(item)
                         print('Dispense command sent')
 
-            if not currentlyDispensing time.sleep(5)
-            if currentlyDispensing:
-                if dispenseTimer == 0 && dispensedPills > item.numberOfPills:
-                    GPIO.output(enable_pin,1)
-                    GPIO.output(buzzer_pin,1)
-                    dispense(3/1000.0,128)
-
-                time.sleep(0.1)
-                dispenseTimer++
-
-                if dispenseTimer > 30 and dispensedPills > item.numberOfPills:
-                    dispensedPills++
-                    dispenseTimer = 0
-                    GPIO.output(buzzer_pin,0)
-                    if not GPIO.input(tripwire_input_pin):
-                        self.queue.put('dispenseError')
-                        dispensedPills = 0
-                        dispenseTimer == 0
-                        currentlyDispensing = False
-                    GPIO.output(enable_pin,0)
-                    time.sleep(0.1)
-                    if chamber_led_input_pin:
-                        self.queue.put('dispenseError')
-                        dispensedPills = 0
-                        dispenseTimer == 0
-                        currentlyDispensing = False
-
-
-
-                else:
-                    #Remove used dispenseItem
-                    dispensedPills = 0
-                    clearEvents('item', msg)
-                    self.queue.put('update')
-                    currentlyDispensing = False
-
+            time.sleep(5)
         print("dispenseCheck worker dying")
 
     def calendarWorkerThread(self):
